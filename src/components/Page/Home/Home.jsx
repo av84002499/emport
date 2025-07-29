7e143a9e95e885b5fc7165a2d2891f6c9091ea56
ANKIT VERMA <109796002+av84002499@users.noreply.github.com>
2025-07-29 18:01:17 +0530

    update

--- src/components/Page/Home/Home.jsx
+++ src/components/Page/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import Servcs from '../Servics/Servics'
+import Servcs from "../Servics/Servics";
 import About from "../About/About";
 const Home = () => {
   return (
@@ -9,7 +9,6 @@ const Home = () => {
         <div className="home-content">
           <h1 className="title">
             <span className="highlight">Chaudhary Global Trade</span>
-            
           </h1>
           <h6 className="subtitle">
             Connecting Worlds, Exporting Opportunities!!
@@ -32,28 +31,29 @@ const Home = () => {
       <Servcs />
 
       <About />
-    
-   <div className="stats-container">
-      <div className="overlay" />
-      <section className="stats-section">
-        <div className="stat-box1">
-          <img src="/images/image5.png" alt="Consignment" />
-          <h2>1500+</h2>
-          <p>Consignment Done</p>
-        </div>
-        <div className="stat-box2">
-          <img src="/images/image5.png" alt="Happy Buyers" />
-          <h2>150+</h2>
-          <p>Happy Buyers</p>
-        </div>
-        <div className="stat-box1">
-          <img src="/images/image5.png" alt="Years Experience" />
-          <h2>10+</h2>
-          <p>Years Experience</p>
-        </div>
-      </section>
-    </div>
 
+      <div className="stats-container">
+        <div className="overlay" />
+        <section className="stats-section">
+          <div className="stat-box1">
+            <img src="/images/image5.png" alt="Consignment" />
+            <h2>1500+</h2>
+            <p>Consignment Done</p>
+          </div>
+          <div className="stat-box2">
+            <img src="/images/image5.png" alt="Happy Buyers" />
+            <h2>150+</h2>
+            <p>Happy Buyers</p>
+          </div>
+          <div className="stat-box1">
+            <img src="/images/image5.png" alt="Years Experience" />
+            <h2>10+</h2>
+            <p>Years Experience</p>
+          </div>
+        </section>
+      </div>
+
+      
     </>
   );
 };
